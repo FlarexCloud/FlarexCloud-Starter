57df93222a418bc232ba791e1063ffd507984f80
TheH2SO4 <theh2so4@protonmail.com>
2022-06-23 11:10:22 +0100

    Update

--- nodejs.sh
+++ nodejs.sh
@@ -25,15 +25,6 @@
 while [ ! -z "$1" ]; do
     case "$1" in
         --file ) FILE="$2"; shift 2;;
-        --npm-script )
-            if [[ $2 != --* ]]; then
-                NPM_SCRIPT="$2"
-                shift
-            else
-                NPM_SCRIPT=""
-            fi
-            shift
-            ;;
         
         --repo )
             if [[ $2 != --* ]]; then
@@ -67,7 +58,6 @@ done
 
 echo -e "${LIGHT_MAGENTA}************************************************************${DEFAULT}"
 echo -e "| > Starter File: '${UNDERLINE}${FILE}\e[24m'"
-echo -e "| > NPM Script '${UNDERLINE}${NPM_SCRIPT}\e[24m'"
 echo -e "| > Git Repository: '${UNDERLINE}${REPO}\e[24m'"
 echo -e "| > Git Branch: '${UNDERLINE}${BRANCH}\e[24m'"
 echo -e "| > Package Manager '${UNDERLINE}${MANAGER}\e[24m'"
@@ -81,13 +71,16 @@ wget -nv -O /tmp/start https://github.com/acidicnodes/starter/raw/master/start.s
 bash /tmp/start "$REPO" "$BRANCH" $SHELL $AUTO_PULL
 
 if [ "$MANAGER" == "ask" ]; then
-    echo "** Please choose a node package manager: [Enter the integer] **"
-    echo "Hint: You can now hide this prompt by setting the default value on the 'Startup' page."
-    echo "1) npm [default]"
-    echo "2) yarn"
-    echo "3) pnpm"
+    echo -e "${LIGHT_MAGENTA}************************************************************${DEFAULT}"
+    echo "-| > Please choose your favourite package manager: [Enter the ${UNDERLINE}integer\e[24m]"
+    echo -e "| > 1) npm [default]"
+    # echo "2) yarn"
+    # echo "3) pnpm"
+    echo -e "-| > ${BOLD}Hint: You could hide this prompt by setting up a default value on the 'Startup' page.${NORMAL}"
+    echo -e "${LIGHT_MAGENTA}************************************************************${DEFAULT}"
     read confirmation
     case $confirmation in
+        """
         2 )
             MANAGER="yarn"
             echo "Using yarn"
@@ -96,40 +89,45 @@ if [ "$MANAGER" == "ask" ]; then
             MANAGER="pnpm"
             echo "Using pnpm"
             ;;
+        """
         * )
             MANAGER="npm"
-            echo "Using npm"
+            echo "*| > Using '${LIGHT_GREEN}NPM${NORMAL}'..."
             ;;
     esac
 fi
 
 if [ -f package.json ] && [ "$AUTO_INSTALL" != "no" ]; then
     if [ "$AUTO_INSTALL" == "ask" ]; then
-        echo "** package.json detected. Continue to install/upgrade from package.json? [Enter yes or no] **"
-        echo "Hint: You can now hide this prompt by setting the default value on the 'Startup' page."
+        echo -e "${LIGHT_MAGENTA}************************************************************${DEFAULT}"
+        echo -e "-| > A package.json have been detected."
+        echo -e "-| > Continue to Install/Upgrade from package.json? [Enter ${UNDERLINE}yes\e[24m or ${UNDERLINE}no\e[24m]"
+        echo -e "-| > ${BOLD}Hint: You could hide this prompt by setting up a default value on the 'Startup' page.${NORMAL}"
+        echo -e "${LIGHT_MAGENTA}************************************************************${DEFAULT}"
         read confirmation
         case $confirmation in
             [Yy]* )
-                echo "Installing/upgrading packages..."
+                echo "*| > Installing/Upgrading from package.json..."
                 eval $MANAGER "install"
                 ;;
-            * ) echo "Skipped!";;
+            * ) echo "*| > Skipped!";;
         esac
     else
         eval $MANAGER "install"
     fi
 fi
 
-echo "Starting app..."
+toilet --filter border:gay AcidicNodes
+echo
+
+sleep 1
+
+echo -e "${LIGHT_MAGENTA}************************************************************${DEFAULT}"
+echo -e "-| > Starting Application/Bot..."
+echo -e "${LIGHT_MAGENTA}************************************************************${DEFAULT}"
 
 CMD="node $FILE"
 
-if [ ! -z "$NPM_SCRIPT" ]; then
-    CMD="npm run $NPM_SCRIPT"
-fi
-
 if [ "$LOGGER" == "yes" ]; then
-    CMD="$CMD | tee alaister_debug_$(date +%d-%m-%Y_%H-%M-%S).log"
+    CMD="$CMD | tee acidicnodes_debug_$(date +%d-%m-%Y_%H-%M-%S).log"
 fi
-
-eval env NODE_EXTRA_CA_CERTS=/tmp/alaister.ca.pem $CMD 
