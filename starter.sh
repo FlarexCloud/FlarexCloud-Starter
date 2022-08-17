57cd7071547ee1707d1706f2220eafa9e805e99f
TheH2SO4 <69685986+theh2so4@users.noreply.github.com>
2022-08-17 11:11:54 +0100

    Update

--- starter.sh
+++ starter.sh
@@ -122,7 +122,7 @@ if [ -d .git ]; then
             fi
         fi
     fi
-elif [ ! -z "$GIT_REPOSITORY" ]; then
+elif [ ! -z "$GIT_REPOSITORY" ] && [ ! -z "$GIT_BRANCH" ]; then
     echo -e "${LIGHT_MAGENTA}************************************************************${DEFAULT}"
     echo -e "| > ${BOLD}WARNING! By cloning a Git Repository, all existing files will be deleted. Continue? [Enter ${UNDERLINE}yes\e[24m or ${UNDERLINE}no\e[24m]"
     echo -e "${LIGHT_MAGENTA}************************************************************${DEFAULT}"
@@ -130,22 +130,15 @@ elif [ ! -z "$GIT_REPOSITORY" ]; then
     case $confirmation in
         [Yy]* )
             rm -rf ..?* .[!.]* *
+            
             echo -e "${LIGHT_MAGENTA}************************************************************${DEFAULT}"
             echo -e "-| > ${BOLD}/home/container${NORMAL} have been wiped out."
             echo -e "-| > Cloning '${LIGHT_GREEN}${GIT_BRANCH}${NORMAL}' from '${LIGHT_GREEN}${GIT_REPOSITORY}${NORMAL}'\e[24m"
             echo -e "${LIGHT_MAGENTA}************************************************************${DEFAULT}"
             if [ ! -z "$GIT_TOKEN" ]; then
-                if [ ! -z "$GIT_BRANCH" ];then
-                    git clone --single-branch --branch ${GIT_BRANCH} https://${GIT_TOKEN}@$(echo -e ${GIT_REPOSITORY} | cut -d/ -f3-) .
-                else
-                    git clone https://${GIT_TOKEN}@$(echo -e ${GIT_REPOSITORY} | cut -d/ -f3-) .
-                fi
+                git clone --single-branch --branch ${GIT_BRANCH} https://${GIT_TOKEN}@$(echo -e ${GIT_REPOSITORY} | cut -d/ -f3-) .
             else
-                if [ ! -z "$GIT_BRANCH" ];then
-                    git clone --single-branch --branch ${GIT_BRANCH} ${GIT_REPOSITORY} .
-                else
-                    git clone ${GIT_REPOSITORY} .
-                fi
+                git clone --single-branch --branch ${GIT_BRANCH} ${GIT_REPOSITORY} .
             fi
             ;;
         * ) echo "*| > Skipped!";;
