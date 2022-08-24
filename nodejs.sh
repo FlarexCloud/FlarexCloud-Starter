20d0bedcaa750788aa3f9be29fc87545e646b2e8
TheH2SO4 <69685986+theh2so4@users.noreply.github.com>
2022-08-24 14:40:21 +0100

    Update

--- nodejs.sh
+++ nodejs.sh
@@ -26,7 +26,6 @@
 
 INSTALLER_VERSION=1.1.1
 DEBIAN_VERSION=$(cat /etc/debian_version)
-BASH_VERSION=$(bash --version)
 GIT_VERSION=$(git --version)
 NODEJS_VERSION=$(node -v)
 PYTHON3_VERSION=$(python3 --version)
