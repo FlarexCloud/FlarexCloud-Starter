f4165b87de1a30edb6df841f538edf262ec68735
TheH2SO4 <theh2so4@protonmail.com>
2024-05-29 14:19:55 +0200

    Update

--- Applications/nodejs.sh
+++ Applications/nodejs.sh
@@ -25,10 +25,10 @@
 # const { exec } = require('child_process');
 
 INSTALLER_VERSION=0.0.1
-DEBIAN_VERSION=$(cat /etc/debian_version)
+OS_VERSION=$(cat /etc/alpine-release)
 GIT_VERSION=$(git --version)
-NODEJS_VERSION=$(node -v)
-PYTHON3_VERSION=$(python3 --version)
+NODE_VERSION=$(node -v)
+PYTHON_VERSION=$(python3 --version)
 NPM_VERSION=$(npm -v)
 YARN_VERSION=$(yarn -v)
 
@@ -63,11 +63,11 @@ fi
 echo -e "${LIGHT_MAGENTA}************************************************************${DEFAULT}"
 echo -e "| > Application: '${UNDERLINE}${APPLICATION}\e[24m'"
 echo -e "| > Starter File: '${UNDERLINE}${FILE}\e[24m'"
+echo -e "| > Package Manager: '${UNDERLINE}${MANAGER}\e[24m'"
+echo -e "| > Terminal Mode: '${UNDERLINE}${TERMINAL}\e[24m'"
 echo -e "| > Git Repository: '${UNDERLINE}${GIT_REPOSITORY}\e[24m'"
 echo -e "| > Git Branch: '${UNDERLINE}${GIT_BRANCH}\e[24m'"
 echo -e "| > Git Token: '${UNDERLINE}${GIT_TOKEN}\e[24m'"
-echo -e "| > Package Manager: '${UNDERLINE}${MANAGER}\e[24m'"
-echo -e "| > Terminal Mode: '${UNDERLINE}${TERMINAL}\e[24m'"
 echo -e "| > Auto Install: '${UNDERLINE}${AUTO_INSTALL}\e[24m'"
 echo -e "| > Auto Pull: '${UNDERLINE}${AUTO_PULL}\e[24m'"
 echo -e "| > Logs: '${UNDERLINE}${LOGGER}\e[24m'"
@@ -80,7 +80,7 @@ echo -e "${LIGHT_MAGENTA}*******************************************************
 echo -e "| > Time Zone: '${UNDERLINE}${TZ}\e[24m'"
 echo -e "| > Cluster: '${UNDERLINE}${P_SERVER_LOCATION}\e[24m'"
 echo -e "| > RAM: '${UNDERLINE}${SERVER_MEMORY}MB\e[24m'"
-echo -e "| > Local IPv4: '${UNDERLINE}${SERVER_IP}\e[24m'"
+echo -e "| > IPv4: '${UNDERLINE}${SERVER_IP}\e[24m'"
 echo -e "| > Primary Port: '${UNDERLINE}${SERVER_PORT}\e[24m'"
 echo -e "| > UUID: '${UNDERLINE}${P_SERVER_UUID}\e[24m'"
 echo -e "${LIGHT_MAGENTA}************************************************************${DEFAULT}"
@@ -90,16 +90,12 @@ echo
 
 echo -e "${LIGHT_MAGENTA}************************************************************${DEFAULT}"
 echo -e "| > Installer Version: '${UNDERLINE}${INSTALLER_VERSION}\e[24m'"
-echo -e "| > Debian Version: '${UNDERLINE}${DEBIAN_VERSION}\e[24m'"
-echo -e "| > Bash Version: '${UNDERLINE}${BASH_VERSION}\e[24m'"
+echo -e "| > Alpine Version: '${UNDERLINE}${OS_VERSION}\e[24m'"
+# echo -e "| > Bash Version: '${UNDERLINE}${BASH_VERSION}\e[24m'" # As we´re using Alpine
 echo -e "| > Git Version: '${UNDERLINE}${GIT_VERSION}\e[24m'"
 echo -e "| > NodeJs Version: '${UNDERLINE}${NODE_VERSION}\e[24m'"
-echo -e "| > Python(3) Version: '${UNDERLINE}${PYTHON3_VERSION}\e[24m'"
-if [ "$NODEJS_VERSION" == "v8.17.0" ] || [ "$NODEJS_VERSION" == "v10.24.1" ] || [ "$NODEJS_VERSION" == "v11.15.0" ]; then
-    echo -e "| > NPM VERSION: '${LIGHT_RED}${UNDERLINE}Not available\e[24m'"
-    echo -e "| > YARN VERSION: '${LIGHT_RED}${UNDERLINE}Not available\e[24m'"
-    echo -e "| > JDK: '${LIGHT_RED}${UNDERLINE}Not available\e[24m'"
-elif [ "$NODEJS_VERSION" == "v16.16.0" ]; then
+echo -e "| > Python Version: '${UNDERLINE}${PYTHON_VERSION}\e[24m'"
+if [ "$NODE_VERSION" == "v16.16.0" ]; then
     echo -e "| > NPM VERSION: '${LIGHT_GREEN}${UNDERLINE}${NPM_VERSION}\e[24m'"
     echo -e "| > YARN VERSION: '${LIGHT_GREEN}${UNDERLINE}${YARN_VERSION}\e[24m'"
     echo -e "| > JDK: '${LIGHT_GREEN}${UNDERLINE}Available\e[24m'"
@@ -110,16 +106,9 @@ else
 fi
 echo -e "${LIGHT_MAGENTA}************************************************************${DEFAULT}"
 
+wget -nv -O /tmp/start https://raw.githubusercontent.com/FlarexCloud/starter/main/starter.sh &> /dev/null
+
 sleep 1.5
-echo
-
-
-echo
-echo -e "${LIGHT_GREEN}************************************************************${DEFAULT}"
-echo
-wget -nv -O /tmp/start https://raw.githubusercontent.com/FlarexCloud/starter/main/starter.sh
-echo
-echo -e "${LIGHT_GREEN}************************************************************${DEFAULT}"
 echo
 
 if [ "$APPLICATION" == "none" ]; then
@@ -129,7 +118,7 @@ if [ "$APPLICATION" == "none" ]; then
     echo
     echo -e "${LIGHT_GREEN}************************************************************${DEFAULT}"
     echo
-elif [ "$APPLICATION" == "-(  Discord Bots  )-" ] || [ "$APPLICATION" == "-(  Telegram Bots  )-" ] || [ "$APPLICATION" == "-(  Twitch Bots  )-" ] || [ "$APPLICATION" == "-(  WhatsApp Bots  )-" ] || [ "$APPLICATION" == "-(  [MD] WhatsApp Bots  )-" ] || [ "$APPLICATION" == "-(  [NON-MD] WhatsApp Bots  )-" ] || [ "$APPLICATION" == "-(  General Applications  )-" ]; then
+elif [ "$APPLICATION" == "-(  WhatsApp Bots  )-" ] || [ "$APPLICATION" == "-(  Discord Bots  )-" ] || [ "$APPLICATION" == "-(  Telegram Bots  )-" ] || [ "$APPLICATION" == "-(  Twitch Bots  )-" ] || [ "$APPLICATION" == "-(  General Applications  )-" ]; then
     echo -e "${LIGHT_RED}************************************************************${DEFAULT}"
     echo
     echo -e "${LIGHT_MAGENTA}************************************************************${DEFAULT}"
@@ -139,7 +128,7 @@ elif [ "$APPLICATION" == "-(  Discord Bots  )-" ] || [ "$APPLICATION" == "-(  Te
     echo -e "${LIGHT_RED}************************************************************${DEFAULT}"
     echo
         exit 0
-elif [ "$APPLICATION" != "-(  Discord Bots  )-" ] || [ "$APPLICATION" != "-(  Telegram Bots  )-" ] || [ "$APPLICATION" != "-(  Twitch Bots  )-" ] || [ "$APPLICATION" != "-(  WhatsApp Bots  )-" ] || [ "$APPLICATION" == "-(  [MD] WhatsApp Bots  )-" ] || [ "$APPLICATION" != "-(  [NON-MD] WhatsApp Bots  )-" ] || [ "$APPLICATION" != "-(  General Applications  )-" ]; then
+elif [ "$APPLICATION" != "-(  WhatsApp Bots  )-" ] || [ "$APPLICATION" != "-(  Discord Bots  )-" ] || [ "$APPLICATION" != "-(  Telegram Bots  )-" ] || [ "$APPLICATION" != "-(  Twitch Bots  )-" ] || [ "$APPLICATION" != "-(  General Applications  )-" ]; then
     echo -e "${LIGHT_RED}************************************************************${DEFAULT}"
     echo
     echo -e "${LIGHT_MAGENTA}************************************************************${DEFAULT}"
@@ -152,37 +141,26 @@ fi
 
 bash /tmp/start "$GIT_REPOSITORY" "$GIT_BRANCH" "$GIT_TOKEN" $TERMINAL $AUTO_PULL
 
-if [ "$NODEJS_VERSION" != "v8.17.0" ] || [ "$NODEJS_VERSION" != "v10.24.1" ] || [ "$NODEJS_VERSION" != "v11.15.0" ]; then
+if [ "$NODE_VERSION" != "v8.17.0" ] || [ "$NODE_VERSION" != "v10.24.1" ] || [ "$NODE_VERSION" != "v11.15.0" ]; then
     npm i yarn@latest
-
-    if [ "$APPLICATION" == "none" ]; then
-        yarn add @adiwajshing/baileys
-    fi
-fi
-
-if [ "$MANAGER" == "yarn" ]; then
-    if [ "$NODEJS_VERSION" == "v8.17.0" ] || [ "$NODEJS_VERSION" == "v10.24.1" ] || [ "$NODEJS_VERSION" == "v11.15.0" ]; then
-        echo -e "${LIGHT_RED}************************************************************${DEFAULT}"
-        echo "-| > Yarn could eventually not work with this NodeJs version. ($NODEJS_VERSION)"
-        echo -e "${LIGHT_RED}************************************************************${DEFAULT}"
-    fi
+    # if [ "$APPLICATION" == "none" ]; then
+        # yarn add @adiwajshing/baileys
+    # fi
 fi
 
 if [ "$MANAGER" == "ask" ]; then
     echo -e "${LIGHT_MAGENTA}************************************************************${DEFAULT}"
     echo "-| > Please choose your favourite package manager: [Enter the integer]"
     echo "-|"
-    if [ "$NODEJS_VERSION" == "v8.17.0" ] || [ "$NODEJS_VERSION" == "v10.24.1" ] || [ "$NODEJS_VERSION" == "v11.15.0" ]; then
+    if [ "$NODE_VERSION" == "v8.17.0" ] || [ "$NODE_VERSION" == "v10.24.1" ] || [ "$NODE_VERSION" == "v11.15.0" ]; then
         echo -e ">> | > *) No package manager can be selected."
     else
         echo -e ">> | > 1) npm ($NPM_VERSION) [default]"
         echo -e ">> | > 2) yarn ($YARN_VERSION)"
     fi
-    # echo -e ">> | > 3) pnpm"
     echo "-|"
     echo -e "-| > ${BOLD}Hint: You could hide this prompt by setting up a default value on the 'Startup' page.${NORMAL}"
     echo -e "${LIGHT_MAGENTA}************************************************************${DEFAULT}"
-    if [ "$NODEJS_VERSION" != "v8.17.0" ] || [ "$NODEJS_VERSION" != "v10.24.1" ] || [ "$NODEJS_VERSION" != "v11.15.0" ]; then
         read confirmation
         case $confirmation in
             2 )
@@ -198,10 +176,6 @@ if [ "$MANAGER" == "ask" ]; then
                 echo
                 sleep 1.5
                 ;;
-            # 3 )
-            #     MANAGER="pnpm"
-            #     echo "Using pnpm"
-            #     ;;
             * )
                 MANAGER="npm"
                 echo
@@ -216,7 +190,6 @@ if [ "$MANAGER" == "ask" ]; then
                 sleep 1.5
                 ;;
         esac
-    fi
 fi
 
 if [ -f package.json ] && [ "$AUTO_INSTALL" != "no" ]; then
@@ -278,6 +251,7 @@ echo
 
 if [ "$APPLICATION" == "none" ]; then
     CMD="node $FILE"
+: <<'END_COMMENT'
 elif [ "$APPLICATION" == "Jareer12/DiscordBotPanel" ] && [ "$SERVER_MEMORY" -lt "2048" ]; then
     echo -e "${LIGHT_RED}************************************************************${DEFAULT}"
     echo
@@ -294,6 +268,7 @@ elif [ "$APPLICATION" != "none" ]; then
     else
         CMD="node ."
     fi
+END_COMMENT
 fi
 
 if [ "$LOGGER" == "yes" ]; then
